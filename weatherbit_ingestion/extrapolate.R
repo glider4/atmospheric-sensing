867b4a0b6627560186c8ab7b207a26bca991b5bf
mathemacode <43391344+mathemacode@users.noreply.github.com>
2019-11-18 21:21:36 -0500

    Readability fix

--- weatherbit_ingestion/extrapolate.R
+++ weatherbit_ingestion/extrapolate.R
@@ -13,7 +13,7 @@ raw_data <- read_csv("weatherlog_2_test_decimals.txt", skip = 1)
 # Clean up, prepare for plots / extrapolation
 names(raw_data) <- c("date", "temp_c", "rel_humid", "press_hpa")
 
-d_idx <- which(names(raw_data) == "date")                 # Index of influenced factor (no rounding needed)
+d_idx <- which(names(raw_data) == "date")             # Index of influenced factor (no rounding needed)
 raw_data[,-d_idx] <- round(raw_data[,-d_idx], 2)      # Round rest of data to 2 decimal places
 raw_data$date <- gsub('\\[|\\]', '', raw_data$date)   # Remove TeraTerm's [ ] format for timestamp
 raw_data$date <- ymd_hms(raw_data$date)               # Implement Lubridate for datetimes
@@ -33,5 +33,3 @@ dewpnt_c = (b * alpha) / (a - alpha)
 # Mutate new column onto df with dewpoint in Celcius
 df <- df %>% 
   mutate("dewpoint" = round(dewpnt_c,2))
-
-
